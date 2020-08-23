88b5136d594c9e18254e06179750f114478ca602
MORIMORI <morimori.0317@outlook.jp>
2020-08-23 20:05:51 +0900

    fix sound

--- src/main/java/net/morimori/yjsnpimod/YJSoundEvents.java
+++ src/main/java/net/morimori/yjsnpimod/YJSoundEvents.java
@@ -2,150 +2,84 @@ package net.morimori.yjsnpimod;
 
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
-import net.minecraftforge.registries.IForgeRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class YJSoundEvents {
-    public static final SoundEvent YJ_BUTTIPA = newSound("yj.buttipa");
-    public static final SoundEvent YJ_DASOUTOOMOEBA = newSound("yj.dasoutoomoeba");
-    public static final SoundEvent YJ_DEDEMASUYO = newSound("yj.dedemasuyo");
-    public static final SoundEvent YJ_FA = newSound("yj.fa");
-    public static final SoundEvent YJ_FOOKIMATII = newSound("yj.fookimatii");
-    public static final SoundEvent YJ_GAKUSEIDESU = newSound("yj.gakuseidesu");
-    public static final SoundEvent YJ_HORAHORAHORA = newSound("yj.horahorahora");
-    public static final SoundEvent YJ_HUN = newSound("yj.hun");
-    public static final SoundEvent YJ_IIYOKOIYO = newSound("yj.iiyokoiyo");
-    public static final SoundEvent YJ_IKISUGI = newSound("yj.ikisugi");
-    public static final SoundEvent YJ_KATAKU = newSound("yj.kataku");
-    public static final SoundEvent YJ_MAZUUTISAXA = newSound("yj.mazuutisaxa");
-    public static final SoundEvent YJ_NIJUYONSAI = newSound("yj.nijuyonsai");
-    public static final SoundEvent YJ_NNA = newSound("yj.nna");
-    public static final SoundEvent YJ_NU = newSound("yj.nu");
-    public static final SoundEvent YJ_NUWATIKARETA = newSound("yj.nuwatikareta");
-    public static final SoundEvent YJ_ON = newSound("yj.on");
-    public static final SoundEvent YJ_ONAON = newSound("yj.onaon");
-    public static final SoundEvent YJ_OSUONEGAI = newSound("yj.osuonegai");
-    public static final SoundEvent YJ_SYU = newSound("yj.syu");
-    public static final SoundEvent YJ_TAIZYUANDSINTYOU = newSound("yj.taizyuandsintyou");
-    public static final SoundEvent YJ_YARIMASUNEXE = newSound("yj.yarimasunexe");
-    public static final SoundEvent YJ_YOKUWAKARANAKATTADESU = newSound("yj.yokuwakaranakattadesu");
-    public static final SoundEvent YJ_ZA = newSound("yj.za");
-    public static final SoundEvent YJ_OTTODAIJOUBUKA = newSound("yj.ottodaijoubuka");
-    public static final SoundEvent YJ_BUTTIPA_MIZIKAME = newSound("yj.buttipa_mizikame");
+
+    public static List<SoundEvent> MOD_SOUNDS = new ArrayList<SoundEvent>();
+
+    public static final SoundEvent YJ_BUTTIPA = register("yj.buttipa");
+    public static final SoundEvent YJ_DASOUTOOMOEBA = register("yj.dasoutoomoeba");
+    public static final SoundEvent YJ_DEDEMASUYO = register("yj.dedemasuyo");
+    public static final SoundEvent YJ_FA = register("yj.fa");
+    public static final SoundEvent YJ_FOOKIMATII = register("yj.fookimatii");
+    public static final SoundEvent YJ_GAKUSEIDESU = register("yj.gakuseidesu");
+    public static final SoundEvent YJ_HORAHORAHORA = register("yj.horahorahora");
+    public static final SoundEvent YJ_HUN = register("yj.hun");
+    public static final SoundEvent YJ_IIYOKOIYO = register("yj.iiyokoiyo");
+    public static final SoundEvent YJ_IKISUGI = register("yj.ikisugi");
+    public static final SoundEvent YJ_KATAKU = register("yj.kataku");
+    public static final SoundEvent YJ_MAZUUTISAXA = register("yj.mazuutisaxa");
+    public static final SoundEvent YJ_NIJUYONSAI = register("yj.nijuyonsai");
+    public static final SoundEvent YJ_NNA = register("yj.nna");
+    public static final SoundEvent YJ_NU = register("yj.nu");
+    public static final SoundEvent YJ_NUWATIKARETA = register("yj.nuwatikareta");
+    public static final SoundEvent YJ_ON = register("yj.on");
+    public static final SoundEvent YJ_ONAON = register("yj.onaon");
+    public static final SoundEvent YJ_OSUONEGAI = register("yj.osuonegai");
+    public static final SoundEvent YJ_SYU = register("yj.syu");
+    public static final SoundEvent YJ_TAIZYUANDSINTYOU = register("yj.taizyuandsintyou");
+    public static final SoundEvent YJ_YARIMASUNEXE = register("yj.yarimasunexe");
+    public static final SoundEvent YJ_YOKUWAKARANAKATTADESU = register("yj.yokuwakaranakattadesu");
+    public static final SoundEvent YJ_ZA = register("yj.za");
+    public static final SoundEvent YJ_OTTODAIJOUBUKA = register("yj.ottodaijoubuka");
+    public static final SoundEvent YJ_BUTTIPA_MIZIKAME = register("yj.buttipa_mizikame");
 
 
-    public static final SoundEvent YJ_BREAK = newSound("yj.break");
-    public static final SoundEvent YJ_PLACE = newSound("yj.place");
-    public static final SoundEvent YJ_STEP = newSound("yj.step");
-    public static final SoundEvent YJ_HIT = newSound("yj.hit");
-    public static final SoundEvent YJ_FALL = newSound("yj.fall");
-    public static final SoundEvent YJ_EQUIP = newSound("yj.equip");
+    public static final SoundEvent YJ_BREAK = register("yj.break");
+    public static final SoundEvent YJ_PLACE = register("yj.place");
+    public static final SoundEvent YJ_STEP = register("yj.step");
+    public static final SoundEvent YJ_HIT = register("yj.hit");
+    public static final SoundEvent YJ_FALL = register("yj.fall");
+    public static final SoundEvent YJ_EQUIP = register("yj.equip");
 
-    public static final SoundEvent YJ_PROLIFERATION = newSound("yj.proliferation");
-    public static final SoundEvent YJ_ANTIPROLIFERATION = newSound("yj.antiproliferation");
-    public static final SoundEvent YJ_DAMEGE = newSound("yj.damege");
-    public static final SoundEvent YJ_OVERWRITE = newSound("yj.overwrite");
-    public static final SoundEvent YJ_ATTACK = newSound("yj.attack");
+    public static final SoundEvent YJ_PROLIFERATION = register("yj.proliferation");
+    public static final SoundEvent YJ_ANTIPROLIFERATION = register("yj.antiproliferation");
+    public static final SoundEvent YJ_DAMEGE = register("yj.damege");
+    public static final SoundEvent YJ_OVERWRITE = register("yj.overwrite");
+    public static final SoundEvent YJ_ATTACK = register("yj.attack");
 
-    public static final SoundEvent MEZIKARA_ALL = newSound("mezikara.all");
-    public static final SoundEvent MEZIKARA_N1 = newSound("mezikara.n1");
-    public static final SoundEvent MEZIKARA_N2 = newSound("mezikara.n2");
-    public static final SoundEvent MEZIKARA_N3 = newSound("mezikara.n3");
-    public static final SoundEvent MEZIKARA_HONTAI = newSound("mezikara.hontai");
-    public static final SoundEvent MEZIKARA_MAEBURI = newSound("mezikara.maeburi");
-    public static final SoundEvent MEZIKARA_NS = newSound("mezikara.ns");
+    public static final SoundEvent MEZIKARA_ALL = register("mezikara.all");
+    public static final SoundEvent MEZIKARA_N1 = register("mezikara.n1");
+    public static final SoundEvent MEZIKARA_N2 = register("mezikara.n2");
+    public static final SoundEvent MEZIKARA_N3 = register("mezikara.n3");
+    public static final SoundEvent MEZIKARA_HONTAI = register("mezikara.hontai");
+    public static final SoundEvent MEZIKARA_MAEBURI = register("mezikara.maeburi");
+    public static final SoundEvent MEZIKARA_NS = register("mezikara.ns");
 
-    public static final SoundEvent TON_ALL = newSound("ton.all");
-    public static final SoundEvent TON_SEKAINOTON = newSound("ton.sekainoton");
-    public static final SoundEvent TON_A1 = newSound("ton.a1");
-    public static final SoundEvent TON_A2 = newSound("ton.a2");
-    public static final SoundEvent TON_A3 = newSound("ton.a3");
-    public static final SoundEvent TON_A4 = newSound("ton.a4");
-    public static final SoundEvent TON_STAGGER = newSound("ton.stagger");
+    public static final SoundEvent TON_ALL = register("ton.all");
+    public static final SoundEvent TON_SEKAINOTON = register("ton.sekainoton");
+    public static final SoundEvent TON_A1 = register("ton.a1");
+    public static final SoundEvent TON_A2 = register("ton.a2");
+    public static final SoundEvent TON_A3 = register("ton.a3");
+    public static final SoundEvent TON_A4 = register("ton.a4");
+    public static final SoundEvent TON_STAGGER = register("ton.stagger");
 
-    public static final SoundEvent CYCLOPS_POPOPOPO = newSound("cyclops.popopopo");
-    public static final SoundEvent CYCLOPS_AIKISO = newSound("cyclops.aikiso");
-    public static final SoundEvent CYCLOPS_BADY = newSound("cyclops.bady");
-    public static final SoundEvent CYCLOPS_CAPTURE = newSound("cyclops.capture");
-    public static final SoundEvent CYCLOPS_EVERY = newSound("cyclops.every");
-    public static final SoundEvent CYCLOPS_NAZOOTO = newSound("cyclops.nazooto");
-    public static final SoundEvent CYCLOPS_SANDSTOME = newSound("cyclops.sandstome");
+    public static final SoundEvent CYCLOPS_POPOPOPO = register("cyclops.popopopo");
+    public static final SoundEvent CYCLOPS_AIKISO = register("cyclops.aikiso");
+    public static final SoundEvent CYCLOPS_BADY = register("cyclops.bady");
+    public static final SoundEvent CYCLOPS_CAPTURE = register("cyclops.capture");
+    public static final SoundEvent CYCLOPS_EVERY = register("cyclops.every");
+    public static final SoundEvent CYCLOPS_NAZOOTO = register("cyclops.nazooto");
+    public static final SoundEvent CYCLOPS_SANDSTOME = register("cyclops.sandstome");
 
-    public static final SoundEvent CYCLOPS_SANGRASSED = newSound("cyclops.sangrassed");
-
-
-    public static void registerSoundEvent(IForgeRegistry<SoundEvent> r) {
-        registrySoundEvent(r, YJ_BUTTIPA);
-        registrySoundEvent(r, YJ_NNA);
-        registrySoundEvent(r, YJ_DASOUTOOMOEBA);
-        registrySoundEvent(r, YJ_DEDEMASUYO);
-        registrySoundEvent(r, YJ_FA);
-        registrySoundEvent(r, YJ_FOOKIMATII);
-        registrySoundEvent(r, YJ_GAKUSEIDESU);
-        registrySoundEvent(r, YJ_HORAHORAHORA);
-        registrySoundEvent(r, YJ_HUN);
-        registrySoundEvent(r, YJ_IIYOKOIYO);
-        registrySoundEvent(r, YJ_IKISUGI);
-        registrySoundEvent(r, YJ_KATAKU);
-        registrySoundEvent(r, YJ_MAZUUTISAXA);
-        registrySoundEvent(r, YJ_NIJUYONSAI);
-        registrySoundEvent(r, YJ_NNA);
-        registrySoundEvent(r, YJ_NU);
-        registrySoundEvent(r, YJ_NUWATIKARETA);
-        registrySoundEvent(r, YJ_ON);
-        registrySoundEvent(r, YJ_ONAON);
-        registrySoundEvent(r, YJ_OSUONEGAI);
-        registrySoundEvent(r, YJ_SYU);
-        registrySoundEvent(r, YJ_TAIZYUANDSINTYOU);
-        registrySoundEvent(r, YJ_YARIMASUNEXE);
-        registrySoundEvent(r, YJ_YOKUWAKARANAKATTADESU);
-        registrySoundEvent(r, YJ_ZA);
-        registrySoundEvent(r, YJ_OTTODAIJOUBUKA);
-        registrySoundEvent(r, YJ_BUTTIPA_MIZIKAME);
-
-        registrySoundEvent(r, YJ_BREAK);
-        registrySoundEvent(r, YJ_PLACE);
-        registrySoundEvent(r, YJ_STEP);
-        registrySoundEvent(r, YJ_HIT);
-        registrySoundEvent(r, YJ_FALL);
-        registrySoundEvent(r, YJ_EQUIP);
-
-        registrySoundEvent(r, YJ_PROLIFERATION);
-        registrySoundEvent(r, YJ_ANTIPROLIFERATION);
-        registrySoundEvent(r, YJ_DAMEGE);
-        registrySoundEvent(r, YJ_OVERWRITE);
-        registrySoundEvent(r, YJ_ATTACK);
-
-        registrySoundEvent(r, MEZIKARA_ALL);
-        registrySoundEvent(r, MEZIKARA_N1);
-        registrySoundEvent(r, MEZIKARA_N2);
-        registrySoundEvent(r, MEZIKARA_N3);
-        registrySoundEvent(r, MEZIKARA_HONTAI);
-        registrySoundEvent(r, MEZIKARA_MAEBURI);
-        registrySoundEvent(r, MEZIKARA_NS);
-
-        registrySoundEvent(r, TON_ALL);
-        registrySoundEvent(r, TON_SEKAINOTON);
-        registrySoundEvent(r, TON_A1);
-        registrySoundEvent(r, TON_A2);
-        registrySoundEvent(r, TON_A3);
-        registrySoundEvent(r, TON_A4);
-        registrySoundEvent(r, TON_STAGGER);
-
-        registrySoundEvent(r, CYCLOPS_POPOPOPO);
-        registrySoundEvent(r, CYCLOPS_AIKISO);
-        registrySoundEvent(r, CYCLOPS_BADY);
-        registrySoundEvent(r, CYCLOPS_CAPTURE);
-        registrySoundEvent(r, CYCLOPS_EVERY);
-        registrySoundEvent(r, CYCLOPS_NAZOOTO);
-        registrySoundEvent(r, CYCLOPS_SANDSTOME);
-        registrySoundEvent(r, CYCLOPS_SANGRASSED);
-    }
-
-    private static void registrySoundEvent(IForgeRegistry<SoundEvent> r, SoundEvent s) {
-        r.register(s);
-    }
-
-    public static SoundEvent newSound(String name) {
-        return new SoundEvent(new ResourceLocation(YJSNPIMOD.MODID, name)).setRegistryName(name);
+    public static final SoundEvent CYCLOPS_SANGRASSED = register("cyclops.sangrassed");
+    
+    private static SoundEvent register(String name) {
+        SoundEvent se = new SoundEvent(new ResourceLocation(YJSNPIMOD.MODID, name)).setRegistryName(name);
+        MOD_SOUNDS.add(se);
+        return se;
     }
 }
--- src/main/java/net/morimori/yjsnpimod/handler/RegistryHandler.java
+++ src/main/java/net/morimori/yjsnpimod/handler/RegistryHandler.java
@@ -5,9 +5,11 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.potion.Effect;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.morimori.yjsnpimod.YJSoundEvents;
 import net.morimori.yjsnpimod.block.YJBlocks;
 import net.morimori.yjsnpimod.effect.YJEffects;
 import net.morimori.yjsnpimod.entity.YJEntityTypes;
@@ -40,5 +42,10 @@ public class RegistryHandler {
     @SubscribeEvent
     public static void onEntityRegistry(final RegistryEvent.Register<EntityType<?>> e) {
         YJEntityTypes.registerEntity(e.getRegistry());
+    }
+
+    @SubscribeEvent
+    public static void onSoundEventRegistry(final RegistryEvent.Register<SoundEvent> e) {
+        YJSoundEvents.MOD_SOUNDS.forEach(n -> e.getRegistry().register(n));
     }
 }
