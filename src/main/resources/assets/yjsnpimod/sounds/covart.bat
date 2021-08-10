0e54e6f5b769a79f5ac9514a67efd278d98e3ad3
MORIMORI0317 <morimori.0317@outlook.jp>
2021-08-10 18:58:26 +0900

    ikisugi

--- src/main/resources/assets/yjsnpimod/sounds/covart.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-md coverted 
-
-for %%a in (*.ogg) do (
-  ffmpeg -i %%a -ac 1 coverted/%%a
-)
--- src/main/resources/assets/yjsnpimod/sounds/covartblk.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-md coverted
-
-for %%a in (*) do (
-  ffmpeg -i %%a -ac 1 coverted/%%a.ogg
-)
