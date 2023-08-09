c1730390ff44ecc2af19d369f8870e4357ef0bf6
MORIMORI0317 <morimori.0317@outlook.jp>
2023-08-10 07:19:15 +0900

    nna

--- forge/src/main/java/net/morimori0317/yajusenpai/explatform/data/forge/YJDataExpectPlatformImpl.java
+++ forge/src/main/java/net/morimori0317/yajusenpai/explatform/data/forge/YJDataExpectPlatformImpl.java
@@ -3,14 +3,14 @@ package net.morimori0317.yajusenpai.explatform.data.forge;
 import dev.felnull.otyacraftengine.data.provider.IntrinsicHolderTagsProviderWrapper;
 import dev.felnull.otyacraftengine.data.provider.ItemTagProviderWrapper;
 import net.minecraft.world.level.block.Block;
-import net.morimori0317.yajusenpai.forge.handler.DataGenHandler;
+import net.morimori0317.yajusenpai.forge.handler.DataGenHandlerW;
 
 public class YJDataExpectPlatformImpl {
     public static void generateBlockTag(IntrinsicHolderTagsProviderWrapper.IntrinsicTagProviderAccess<Block> providerAccess) {
-        DataGenHandler.generateBlockTag(providerAccess);
+        DataGenHandlerW.generateBlockTag(providerAccess);
     }
 
     public static void generateItemTag(ItemTagProviderWrapper.ItemTagProviderAccess providerAccess) {
-        DataGenHandler.generateItemTag(providerAccess);
+        DataGenHandlerW.generateItemTag(providerAccess);
     }
 }
